9fa29e7941166f9d466c76ab44269b9f6fb1595b
BERKAN OZGUR <berkanozgur@hotmail.com>
2024-05-07 13:48:12 +0300

    bug fixes

--- note_sharing/note_sharing/urls.py
+++ note_sharing/note_sharing/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls'))
-    # going to check /core/urls.py
 ]
 
 urlpatterns = urlpatterns + \
