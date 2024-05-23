10a623e386a7d3678f0a11a9a81237e11274c519
ibrahimardic <ibrahimard2000@gmail.com>
2024-05-23 17:23:45 +0300

    User Suggestions updated

--- note_sharing/core/views.py
+++ note_sharing/core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
 from itertools import chain
+import random
 
 
 # Create your views here.
@@ -29,7 +29,32 @@ def index(request):
     feed_list = list(chain(*feed))
 
     posts = Post.objects.all()  # It returns as a list.
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
+
+    #User suggestion starts
+    all_users = User.objects.all()
+    user_following_all =[]
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if(x not in list(user_following_all))]
+    current_user = User.objects.filter(username = request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user = ids)
+        username_profile_list.append(profile_lists)
+
+    suggestions_username_profile_lists = list(chain(*username_profile_list))
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list, 'suggestions_username_profile_lists':suggestions_username_profile_lists[:4]})
+
 
 
 @login_required(login_url='signin')
